fe821bb47a2568083ce0153a2b3ab556dcf71ae9
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2023-07-15 17:22:04 +0530

    enbable cors

--- src/main/java/com/shivam/todoapp/config/SecurityConfig.java
+++ src/main/java/com/shivam/todoapp/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.shivam.todoapp.config;
 
 import com.shivam.todoapp.service.jwt.JwtTokenFilter;
-import com.shivam.todoapp.utils.JWTUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -34,10 +38,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+        cors.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        cors.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.ACCEPT,
+                HttpHeaders.CACHE_CONTROL,
+                HttpHeaders.CONTENT_TYPE
+        ));
+        cors.setExposedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION));
+        cors.setAllowCredentials(true);
         http.csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/authorize", "/user/register", "/").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().cors().configurationSource(request -> cors);
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
--- src/main/java/com/shivam/todoapp/controller/UserController.java
+++ src/main/java/com/shivam/todoapp/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
 @RequestMapping(path = "/user")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
